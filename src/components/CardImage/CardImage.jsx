1baf21f7892790f1d20132e2d6e8da5ff6fee2c5
Federico Lopez <federico.lopez@delos.com>
2023-06-21 14:34:31 -0300

    refactor code & add CardImage component

--- src/components/CardImage/CardImage.jsx
+++ src/components/CardImage/CardImage.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { IconButton } from "@material-ui/core";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+// Styles
+import useStyles from "./CardImageStyles";
 
-const CardImage = () => {
-  return <div>CardImage</div>;
+const CardImage = ({ photo, onFavoriteClick }) => {
+  const classes = useStyles();
+
+  const handleFavoriteClick = (event) => {
+    event.stopPropagation();
+    onFavoriteClick(photo);
+  };
+
+  return (
+    <div key={photo.id} className={classes.imageWrapper}>
+      <img src={photo.img_src} alt={photo.id} className={classes.image} />
+      <div className={classes.hoverOverlay}>
+        <IconButton aria-label="Add to favorites" onClick={handleFavoriteClick}>
+          <FavoriteIcon className={classes.hoverIcon} />
+        </IconButton>
+      </div>
+    </div>
+  );
 };
 
 export default CardImage;
