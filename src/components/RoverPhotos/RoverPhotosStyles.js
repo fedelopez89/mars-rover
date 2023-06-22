d71c2ace1d53f7cf06d0922d39ce364c8b00cdfa
Federico Lopez <federico.lopez@delos.com>
2023-06-22 19:03:40 -0300

    bugfix Header style for mobile

--- src/components/RoverPhotos/RoverPhotosStyles.js
+++ src/components/RoverPhotos/RoverPhotosStyles.js
@@ -18,6 +18,8 @@ const useStyles = makeStyles((theme) => ({
         width: "100%",
         height: "100%",
         objectFit: "cover",
+        objectPosition: "100%",
+        boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
     },
     loaderContainer: {
         display: "flex",
